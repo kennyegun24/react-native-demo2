dea6e7db13bf7f14f452f3229a73e5405db7a79a
Kenny Egun <kennyegun240@gmail.com>
2023-05-07 21:11:34 +0100

    Modify

--- App.js
+++ App.js
@@ -1,20 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
-import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+import Body from './pages/Body';
+import { Provider } from 'react-redux';
+import store from './redux/store';
+import Navigator from './routes/HomeStack'
 
 export default function App() {
   return (
-    <SafeAreaView style={styles.container}>
-      {/* <StatusBar style="dark" /> */}
-      <Text>Open up App.js to start working on your app!</Text>
-      <Text>Open up App.js to start working on your app!</Text>
-      <Text>Open up App.js to start working on your app!</Text>
-    </SafeAreaView>
+    <Provider store={store}>
+      <SafeAreaView style={styles.container}>
+        <StatusBar style="auto" />
+        <Navigator />
+      </SafeAreaView>
+    </Provider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    // marginTop: 20
+    // backgroundColor: '#fff',
   },
 });
